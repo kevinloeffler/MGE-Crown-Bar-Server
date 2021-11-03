08d2fe5effa189b9f575dd27e35e8726aafe17fe
Kevin Loeffler <hi@kevinloeffler.ch>
2021-11-03 16:38:50 +0100

    Add: Port from Heroku

--- main.mjs
+++ main.mjs
@@ -19,6 +19,6 @@ app.get("/events", (req, res) => {
     res.json(eventsObject)
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server running on port 3000");
 });
